7c7ca7f6bf79200255e37acc9edc7010c5e5721f
Ann <ann.mitrakhovich@gmail.com>
2023-12-19 11:27:30 +0300

    use code of error -1

---
+++ src/task1/Main.java
@@ -0,0 +1,22 @@
+package task1;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int[] array = new int[20];
+        Random random = new Random();
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10);
+        }
+        int min = 15;
+        if (array.length <= min) {
+            System.out.println("-1");
+        } else {
+            System.out.println(array.length);
+        }
+    }
+}
